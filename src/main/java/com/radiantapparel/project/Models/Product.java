fa3613617d543fd7977c12c5fa422788a626dc3e
FJohnHRoss <rossjohn936@gmail.com>
2022-12-16 16:38:47 -0800

    update

--- src/main/java/com/radiantapparel/project/Models/Product.java
+++ src/main/java/com/radiantapparel/project/Models/Product.java
@@ -110,11 +110,11 @@ public class Product {
     }
 
     public String getImage() {
-        return image;
+        return images;
     }
 
-    public void setImage(String image) {
-        this.image = image;
+    public void setImage(String images) {
+        this.images = images;
     }
 
 }
