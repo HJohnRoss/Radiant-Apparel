505dc798354f1e403c9fbde9dbebcf1c200d3f26
Benjamin Ackerman <115006023+ackerm17@users.noreply.github.com>
2022-12-17 16:42:26 -0500

    css folders and dashboard started

--- src/main/java/com/radiantapparel/project/Controllers/DashboardController.java
+++ src/main/java/com/radiantapparel/project/Controllers/DashboardController.java
@@ -1,8 +1,25 @@
 package com.radiantapparel.project.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.radiantapparel.project.Services.ProductService;
 
 
 @Controller
 public class DashboardController {
+
+    @Autowired
+    ProductService productService;
+
+    @GetMapping("/")
+    public String showDashboard(Model model ){
+        
+        model.addAttribute("allProducts", productService.allProducts());
+
+        return "dashboard.jsp";
+    }
+
 }
