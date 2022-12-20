6b2ca42178902f3e1139dee9dc9957a752671488
FJohnHRoss <rossjohn936@gmail.com>
2022-12-19 22:34:38 -0800

    change key

--- src/main/java/com/radiantapparel/project/ProjectApplication.java
+++ src/main/java/com/radiantapparel/project/ProjectApplication.java
@@ -11,7 +11,7 @@ public class ProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
         // John's key
-        Stripe.apiKey = "sk_test_51MFlFOA7FBolyCg75096WXsZfqQ3uAOZSoKUizxtzP2miYf9BYZH0EhtNd8gENoleymHrTmN8GHAYBkeBbq8peZi001eGcYboT";
+        // Stripe.apiKey = "sk_test_51MFlFOA7FBolyCg75096WXsZfqQ3uAOZSoKUizxtzP2miYf9BYZH0EhtNd8gENoleymHrTmN8GHAYBkeBbq8peZi001eGcYboT";
         // Ciaran's key
         // Stripe.apiKey = "sk_test_51MFpnALrPQlCjFmwmbp9TYyuUnyJl5KRKH9aY92qVUgEnioVB4zwrSP1yS6vSW2tZtWKYYRGCO4wGB55E9eK2NmR00ugMxu2aY";
         // Ben's key
